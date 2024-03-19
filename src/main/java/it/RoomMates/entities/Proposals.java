0c148e2a5efe4eff6b58bcb22d707668c46675c5
Maudit00 <davide9311@gmail.com>
2024-03-19 10:52:26 +0100

    added proposal entity

--- src/main/java/it/RoomMates/entities/Proposals.java
+++ src/main/java/it/RoomMates/entities/Proposals.java
@@ -1,22 +1,22 @@
 package it.RoomMates.entities;
 
-import it.RoomMates.enums.Employment;
+import it.RoomMates.enums.PropStatus;
 import jakarta.persistence.*;
 import lombok.Data;
-import org.apache.catalina.User;
 
 @Data
 @Entity
-public class RoomMate {
+public class Proposals {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_utenti")
     @SequenceGenerator(name = "id_utenti", initialValue = 1, allocationSize = 1)
     private int id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "user_id")
     private Users user;
 
+    private String description;
     @Enumerated
-    private Employment employment;
+    private PropStatus status;
 }
---
+++ src/main/java/it/RoomMates/enums/PropStatus.java
@@ -0,0 +1,5 @@
+package it.RoomMates.enums;
+
+public enum PropStatus {
+    PENDING, ACCEPTED, REJECTED
+}
