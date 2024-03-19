fe019afb43d0ffc1bfa63f03dad522953ae9f68f
Maudit00 <davide9311@gmail.com>
2024-03-19 10:26:07 +0100

    init

---
+++ src/main/java/it/RoomMates/RoomMatesApplication.java
@@ -0,0 +1,13 @@
+package it.RoomMates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomMatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/RoomMates/RoomMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.RoomMates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomMatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
